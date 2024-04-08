923cfb7fb307cd3bcefc12af7b621dc24150f8f4
LuisEspitaleta <luis.espitaleta@udea.edu.co>
2024-04-08 10:23:39 -0500

    Creando Funcion rotar matriz

--- main.cpp
+++ main.cpp
@@ -16,13 +16,24 @@ int** crearMatrizCuadrada(int &tamano) {
 
 // Función para imprimir la matriz
 void imprimirMatriz(int** matriz, int &tamano) {
-    cout << "Matriz resultante:\n"; // Usamos cout en lugar de std::cout
     for (int i = 0; i < tamano; ++i) {
         for (int j = 0; j < tamano; ++j) {
             cout << matriz[i][j] << " "; // Usamos cout en lugar de std::cout
         }
         cout << "\n"; // Salto de linea
     }
+}
+
+// Función para rotar la matriz en sentido horario (90 grados)
+int** rotarMatrizCuadrada(int** matriz, int tamano) {
+    int** matrizRotada = new int*[tamano];
+    for (int i = 0; i < tamano; ++i) {
+        matrizRotada[i] = new int[tamano];
+        for (int j = 0; j < tamano; ++j) {
+            matrizRotada[i][j] = matriz[tamano - j - 1][i];
+        }
+    }
+    return matrizRotada;
 }
 
 int main() {
@@ -34,14 +45,24 @@ int main() {
     // Crear la matriz llamando la funcion crearMatrizCuadrada()
     int** miMatriz = crearMatrizCuadrada(*ptrTamano);
 
+    cout << "Matriz original:\n";
     // Imprimir la matriz llamando la funcion imprimirMatriz()
     imprimirMatriz(miMatriz, *ptrTamano);
 
-    // Liberar la memoria por fila
-    for (int i = 0; i < *ptrTamano; ++i) {
+    // Rotar la matriz y obtener la matriz rotada
+    int** matrizRotada = rotarMatrizCuadrada(miMatriz, tamano);
+
+    // Imprimir la matriz rotada
+    cout << "Matriz rotada:\n";
+    imprimirMatriz(matrizRotada, tamano);
+
+    // Liberar la memoria de ambas matrices
+    for (int i = 0; i < tamano; ++i) {
         delete[] miMatriz[i];
+        delete[] matrizRotada[i];
     }
     delete[] miMatriz;
+    delete[] matrizRotada;
 
     return 0;
 }
