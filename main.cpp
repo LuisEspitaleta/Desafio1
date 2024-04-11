8b5074739e490e3bc99a5fba887206c907ce3dff
LuisEspitaleta <luis.espitaleta@udea.edu.co>
2024-04-11 07:11:24 -0500

    Probando llenado aleatorio

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -66,17 +69,39 @@ int compararPosicionMatrices(int** matrizA, int** matrizB, int* llave) {
     return 0;
 }
 
-int llenaLLaveCerradura(int* ptrCantidadMatrices, char* ptrOpcion){
+int** llenaLLaveCerradura(int* ptrCantidadMatrices, int* ptrOpcion){
 
     int tamanioLlave, tamanioCerradura, valorCerradura, valorLlave;
     int *ptrValorCerradura = &valorCerradura;
     int *ptrValorLlave = &valorLlave;
     int *cerraduraX = new int[*ptrCantidadMatrices];
     int *llaveK = new int[*ptrCantidadMatrices+1];
-    string opcion(ptrOpcion);
-    cout << opcion;
-    if (opcion == "S" || opcion == "s") {//Llenar de manera aletoria
+    int** llaveYCerrradura = new int*[2];
 
+    // Inicializar la semilla para la generación de números aleatorios
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    if (*ptrOpcion == 1) {//Llenar de manera aletoria
+        for (int i = 0; i < *ptrCantidadMatrices; ++i) {
+            int valor;
+            do {
+                valor = rand() % 100;
+            } while (valor % 2 == 0 || valor < 3); // Repetir si el número es par o menor que 3
+            cerraduraX[i] = valor;
+            cout << "Valor para la Cerradura[" << i << "]: " << cerraduraX[i];
+        }
+        for (int i = 0; i < *ptrCantidadMatrices+1; ++i) {
+            if (i > 1) {
+                do {
+                    llaveK[i] = (rand() % 3) - 1;
+                } while (*ptrValorLlave > 1 || *ptrValorLlave < -1);
+                cout << "Valor para la llave[" << i << "]: " << llaveK[i];
+            }else {
+                cout << "Ingrese el valor para la llave[" << i << "]: ";
+                cin >> *ptrValorLlave;
+                llaveK[i] = *ptrValorLlave;
+            }
+        }
     }else{//Llenar de manera Manual
         // Llenar el array preguntando al usuario por los valores la cerradura X
         for (int i = 0; i < *ptrCantidadMatrices; ++i) {
@@ -104,31 +129,35 @@ int llenaLLaveCerradura(int* ptrCantidadMatrices, char* ptrOpcion){
         }
     }
 
+    llaveYCerrradura[0] = cerraduraX;
+    llaveYCerrradura[1] = llaveK;
+    return llaveYCerrradura;
+
 }
 
 int main() {
 
-    int tamanioLlave, tamanioCerradura;
+    int tamanioLlave, tamanioCerradura, opcion;
     int cantidadMatrices;
     int *ptrCantidadMatrices = &cantidadMatrices;
-    char opcion;
-    char *ptrOpcion = &opcion;
-    int* arrayOpen = new int[2];
+    int *ptrOpcion = &opcion;
+    int** arrayOpen = new int*[2];
 
     // Preguntar tamaño de la cerradura (debe ser de 3 o más)
     do {
-        cout << "Ingrese el tamaño de la cerradura (debe ser 3 o más): ";
+        cout << "Ingrese el tamaio de la cerradura (debe ser 3 o mas): ";
         cin >> *ptrCantidadMatrices;
     } while (*ptrCantidadMatrices < 3);
     int *cerradura = new int[*ptrCantidadMatrices];
     int *llave = new int[*ptrCantidadMatrices+1];
     do {
-        cout << "Desea generar los datos aleatoriamente? (s/n): ";
+        cout << "Desea generar los datos aleatoriamente? Si(1) No(2): ";
         cin >> *ptrOpcion;
-    } while (*ptrOpcion != 's' && *ptrOpcion != 'S' && *ptrOpcion != 'n' && *ptrOpcion != 'N');
+    } while (*ptrOpcion != 1 && *ptrOpcion != 2);
 
-    *arrayOpen = llenaLLaveCerradura(ptrCantidadMatrices, ptrOpcion);
-
+    arrayOpen = llenaLLaveCerradura(ptrCantidadMatrices, ptrOpcion);
+    cerradura = arrayOpen[0];
+    llave = arrayOpen[1];
 
     cout << "\n";
 
